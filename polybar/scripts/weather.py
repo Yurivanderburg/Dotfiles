a127b257eb12ec241308061987415416516ab3f7
Yuri van der Burg <yuri.vanderburg@gmail.com>
2023-09-18 17:26:51 +0200

    Initial commit

---
+++ polybar/scripts/weather.py
@@ -0,0 +1,56 @@
+import requests
+import json
+import subprocess
+
+
+battery_status = subprocess.check_output('cat /sys/class/power_supply/BAT0/status', shell=True).decode('utf-8').split('\n')[0]
+
+if battery_status == 'Discharging':
+    display_forecast = True
+else: 
+    display_forecast = False
+    
+
+
+
+def main():
+    try:
+        # Get data from weatherapi
+        url = ('https://api.weatherapi.com/v1/current.json?key=ad26958ee93e40f990c154244232806&q=Zurich')
+        result = requests.get(url)
+
+
+        if(result.status_code == requests.codes['ok']):
+            weather = result.json()
+            temp = weather['current']["temp_c"]
+            condition = weather['current']['condition']['text']
+       
+            if not display_forecast:
+                return  f"{condition}, {temp}°C"
+
+        else:
+            return "API Error"
+
+
+        # Grab forecast (if wanted)
+        if display_forecast:
+            url_forecast =  ('https://api.weatherapi.com/v1/forecast.json?key=ad26958ee93e40f990c154244232806&q=Zurich&days=2')
+            result_forecast = requests.get(url_forecast)
+
+            if(result_forecast.status_code == requests.codes['ok']):
+                weather_forecast = result_forecast.json()
+                condition_forecast = weather_forecast['forecast']['forecastday'][1]['day']['condition']['text']
+                temp_forecast = weather_forecast['forecast']['forecastday'][1]['day']['maxtemp_c']
+                #return f"{condition}, {temp}°C > {condition_forecast}, {temp_forecast}°C"
+                return f"{condition}, {temp}°C --> {temp_forecast}°C"
+
+            else:
+                return "API Error" 
+
+
+    except:
+        return "Network Error" 
+
+if __name__ == "__main__":
+    print(main())
+
